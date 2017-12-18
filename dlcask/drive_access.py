0c91af3a8c46e64610b5823549558799b9fd77c6
mrmiguez <r.m.miguez@gmail.com>
2017-12-18 15:31:33 -0800

    batching out projects

--- dlcask/drive_access.py
+++ dlcask/drive_access.py
@@ -3,19 +3,19 @@ import googleapiclient
 from collections import namedtuple
 from secret import FOLDER_ID
 
-project_ids = namedtuple('project_ids', 'name gid wkbk_gid md_gid')
-project_data = namedtuple('project_data', 'title digi_perc qc_perc ready_perc md_perc')
+simple_project = namedtuple('simple_project', 'title batch_data')
+#complex_project = namedtuple('complex_project', 'title batch_nums data_batch_list')
+project_ids = namedtuple('project_ids', 'title gid wkbk_gid md_gid')
+project_data = namedtuple('project_data', 'digi_perc qc_perc ready_perc md_perc')
 
 
 # get DLC Project Folders
-def project_folders(drive_service):
-    projects = []
+def get_projects(drive_service):
     files = drive_service.files().list(
             q="'{0}' in parents and mimeType='application/vnd.google-apps.folder'".format(FOLDER_ID)).execute(
             ).get('files', [])
-    for f in files:
-        projects.append(project_ids(f['name'], f['id'], sheet_ids(drive_service, f['id']), sheet_ids(drive_service, f['id'])))
-    return projects
+    return [project_ids(f['name'], f['id'], sheet_ids(drive_service, f['id'])[0], sheet_ids(drive_service, f['id'])[1])
+            for f in files]
 
 
 # Retrieve spreadsheet ID's
@@ -31,36 +31,57 @@ def sheet_ids(drive_service, parent_gid):
     for f in files:
         if workbook.search(f['name']):
             wkbk_ids.append(f['id'])
-            return wkbk_ids
         if mods.search(f['name']):
             mods_ids.append(f['id'])
-            return mods_ids
+    return wkbk_ids, mods_ids
 
 
 def parse_projects(drive_service, sheet_service):
-    return [project_data(project.name,
-                         "15",
-                         "20",
-                         "25",
-                         md_perc(sheet_service, project.md_gid))
-            for project in project_folders(drive_service)]
+    projects = []
+    project_query = get_projects(drive_service)
+    for project in project_query:
+        if len(project.md_gid) < 2:
+            try:
+                perc1, perc2, perc3, perc4 = data_stats(sheet_service, project.md_gid[0])
+                projects.append(simple_project(project.title,
+                                           [project_data(perc1, perc2, perc3, perc4)]))
+            except IndexError:
+                pass
+        else:
+            data_list = []
+            for gid in project.md_gid:
+                perc1, perc2, perc3, perc4 = data_stats(sheet_service, gid)
+                data_list.append(project_data(perc1, perc2, perc3, perc4))
+            projects.append(simple_project(project.title, data_list))
+    return projects
+
+
+def data_stats(sheet_service, md_gid):
+    return (17, 35, 48, md_perc(sheet_service, md_gid))
+
+
+
+# def parse_projects(drive_service, sheet_service):
+#     return [project_data(project.name,
+#                          "15",
+#                          "20",
+#                          "25",
+#                          md_perc(sheet_service, project.md_gid))
+#             for project in project_folders(drive_service)]
 
 
 def md_perc(sheet_service, sheet_id):
-    if sheet_id is not None and len(sheet_id) < 2:
-        request = sheet_service.spreadsheets().values().get(spreadsheetId=sheet_id[0], range="A1:ZZ99999", majorDimension="COLUMNS")
-        response = request.execute()
-        iid_list = response["values"][0]
-        record_complete_data = response["values"][-2]
-        iid_count = 0
-        record_complete_count = 0
-        for item in iid_list:
-            if item:
-                iid_count = iid_count + 1
-        for item in record_complete_data:
-            if item:
-                record_complete_count = record_complete_count + 1
-        print(iid_list, record_complete_data)  # test line
-        return (record_complete_count / iid_count) * 100
-    else:
-        return 0
+    request = sheet_service.spreadsheets().values().get(spreadsheetId=sheet_id, range="A1:ZZ99999", majorDimension="COLUMNS")
+    response = request.execute()
+    iid_list = response["values"][0]
+    record_complete_data = response["values"][-2]
+    iid_count = 0
+    record_complete_count = 0
+    for item in iid_list:
+        if item:
+            iid_count = iid_count + 1
+    for item in record_complete_data:
+        if item:
+            record_complete_count = record_complete_count + 1
+    return (record_complete_count / iid_count) * 100
+
