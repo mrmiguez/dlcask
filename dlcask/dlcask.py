f6bd069b1b16831e75d153cfebdf2ab3502500ad
mrmiguez <r.m.miguez@gmail.com>
2017-12-18 11:19:17 -0800

    pieces coming together

--- dlcask/dlcask.py
+++ dlcask/dlcask.py
@@ -1,3 +1,4 @@
+import os
 import drive_access
 from collections import namedtuple
 from flask import Flask, render_template
@@ -6,20 +7,16 @@ from httplib2 import Http
 from oauth2client.service_account import ServiceAccountCredentials
 
 app = Flask(__name__)
-#project_data = namedtuple('project_data', 'title digi_perc qc_perc ready_perc md_perc')
 
-# projects = [project_data("Radzinowics", "5", "80", "15", "0"),
-#             project_data("HUA Media Guides", "55", "15", "30", "10"),
-#             project_data("FBCTLH", "0", "0", "100", "65")
-#             ]
+self_path = os.path.abspath(os.path.dirname(__file__))
 
 scopes = (
     'https://www.googleapis.com/auth/drive.metadata.readonly',
     'https://www.googleapis.com/auth/spreadsheets.readonly'
 )
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name(
-    '/home/mrmiguez/bin/dlcask/dlcask/secret/dlcask_service_secret.json', scopes=scopes)
+credentials = ServiceAccountCredentials.from_json_keyfile_name(os.path.join(
+    self_path, 'secret/dlcask_service_secret.json'), scopes=scopes)
 http_auth = credentials.authorize(Http())
 drive_service = discovery.build('drive', 'v3', http=http_auth)
 sheet_service = discovery.build('sheets', 'v4', http=http_auth)
--- dlcask/drive_access.py
+++ dlcask/drive_access.py
@@ -1,8 +1,9 @@
 import re
 import googleapiclient
 from collections import namedtuple
+from secret import FOLDER_ID
 
-project = namedtuple('project', 'name gid wkbk_gid md_gid')
+project_ids = namedtuple('project_ids', 'name gid wkbk_gid md_gid')
 project_data = namedtuple('project_data', 'title digi_perc qc_perc ready_perc md_perc')
 
 
@@ -10,10 +11,10 @@ project_data = namedtuple('project_data', 'title digi_perc qc_perc ready_perc md
 def project_folders(drive_service):
     projects = []
     files = drive_service.files().list(
-            q="'0Byss0UxL3CupS2UzTVhwNmxSLTA' in parents and mimeType='application/vnd.google-apps.folder'").execute(
+            q="'{0}' in parents and mimeType='application/vnd.google-apps.folder'".format(FOLDER_ID)).execute(
             ).get('files', [])
     for f in files:
-        projects.append(project(f['name'], f['id'], sheet_ids(drive_service, f['id'])[0], sheet_ids(drive_service, f['id'])[1]))
+        projects.append(project_ids(f['name'], f['id'], sheet_ids(drive_service, f['id']), sheet_ids(drive_service, f['id'])))
     return projects
 
 
@@ -21,25 +22,19 @@ def project_folders(drive_service):
 def sheet_ids(drive_service, parent_gid):
     workbook = re.compile("WorkBook|workbook|Workbook")
     mods = re.compile("^mods")
-    # request = drive_service.files().list(q="{0} in parents and mimeType='application/vnd.google-apps.spreadsheet'".format(
-    #         parent_gid))
-    # response = request.execute()
-    # files = response.get('files', [])
-    try:
-        request = drive_service.files().list(q="{0} in parents and mimeType='application/vnd.google-apps.spreadsheet'".format(
-                parent_gid))
-        response = request.execute()
-        files = response.get('files', [])
-
-        for f in files:
-            if workbook.search(f['name']):
-                wkbk_id = f['id']
-            if mods.search(f['name']):
-                mods_id = f['id']
-        return wkbk_id, mods_id
-    except googleapiclient.errors.HttpError:
-        return None, None
-
+    request = drive_service.files().list(q="'{0}' in parents and mimeType='application/vnd.google-apps.spreadsheet'".format(
+            parent_gid))
+    response = request.execute()
+    files = response.get('files', [])
+    mods_ids = []
+    wkbk_ids = []
+    for f in files:
+        if workbook.search(f['name']):
+            wkbk_ids.append(f['id'])
+            return wkbk_ids
+        if mods.search(f['name']):
+            mods_ids.append(f['id'])
+            return mods_ids
 
 
 def parse_projects(drive_service, sheet_service):
@@ -52,8 +47,8 @@ def parse_projects(drive_service, sheet_service):
 
 
 def md_perc(sheet_service, sheet_id):
-    if sheet_id is not None:
-        request = sheet_service.spreadsheets().values().get(spreadsheetId=sheet_id, range="A1:ZZ99999", majorDimension="COLUMNS")
+    if sheet_id is not None and len(sheet_id) < 2:
+        request = sheet_service.spreadsheets().values().get(spreadsheetId=sheet_id[0], range="A1:ZZ99999", majorDimension="COLUMNS")
         response = request.execute()
         iid_list = response["values"][0]
         record_complete_data = response["values"][-2]
@@ -65,6 +60,7 @@ def md_perc(sheet_service, sheet_id):
         for item in record_complete_data:
             if item:
                 record_complete_count = record_complete_count + 1
+        print(iid_list, record_complete_data)  # test line
         return (record_complete_count / iid_count) * 100
     else:
         return 0
